d8dda121111a47a58ee343a29446f34c1fb3aa72
composingcap <composingcap@gmail.com>
2019-10-06 22:49:54 -0500

    Update theThing.js

--- theThing.js
+++ theThing.js
@@ -1,3 +1,24 @@
 function changeFrame(name, url){
     document.getElementById(name).src = url;
 }
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function getUrlParam(parameter, defaultvalue){
+    var urlparameter = defaultvalue;
+    if(window.location.href.indexOf(parameter) > -1){
+        urlparameter = getUrlVars()[parameter];
+        }
+    return urlparameter;
+}
+
+var frameChanger = getUrlParam('frame', "home");
+
+
+changeFrame("confusionFrame", "./" + frameChanger +".html");
